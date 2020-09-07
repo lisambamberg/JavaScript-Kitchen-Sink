c8b319d65c21f540ac1125900b77810abf17d185
lisambamberg <lisambamberg@gmail.com>
2020-09-07 16:04:41 -0500

    Added function, array, and object

--- app.js
+++ app.js
@@ -7,9 +7,28 @@ const statesUS = 50;
 let sum = 5 + 4;
 //This is the sum of the two numbers given
 
-function sayHello (){
+function sayHello() {
     alert("Hello World!");
 }
-
 sayHello();
 
+function checkAge(name, age) {
+    if (age < 21) {
+        alert("Sorry " + name + ", you aren't old enough to view this page!");
+    }
+}
+checkAge("Charles", 21);
+checkAge("Abby", 27);
+checkAge("James", 18);
+checkAge("John", 17);
+
+let favVegetables = ["broccoli", "carrots", "beets", "radicchio"];
+for (let i = 0; i < favVegetables.length; i++) {
+    console.log(favVegetables[i]);
+}
+
+let pet = {
+    name: "Milo",
+    breed: "orange tabby"
+}
+console.log(pet);
