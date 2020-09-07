fbcab3b750cb20dbe7cdc1dd276e887b0a70e636
lisambamberg <lisambamberg@gmail.com>
2020-09-07 14:32:55 -0500

    Added variable, constant, and function

---
+++ app.js
@@ -0,0 +1,15 @@
+let firstName = "Lisa";
+//This is a variable with a value assigned to it
+
+const statesUS = 50;
+//This is a constant with a value assigned to it
+
+let sum = 5 + 4;
+//This is the sum of the two numbers given
+
+function sayHello (){
+    alert("Hello World!");
+}
+
+sayHello();
+
